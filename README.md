ecf699c904be5f830aa55d2310e02fa67e7ebf6b
AdamHaniff <AdamHaniffDeveloper@gmail.com>
2023-08-29 16:49:15 -0400

    Styled calculation container for mobile layout

--- README.md
+++ README.md
@@ -7,8 +7,8 @@ Users should be able to:
 • View the optimal layout for the interface depending on their device's screen size  
 • See hover and focus states for all interactive elements on the page  
 • Receive validation errors if:  
- • Any field is empty when the form is submitted  
- • The day number is not between 1-31  
- • The month number is not between 1-12  
- • The year is in the future  
- • The date is invalid e.g. 31/04/1991 (there are 30 days in April)
+&nbsp;&nbsp;• Any field is empty when the form is submitted  
+&nbsp;&nbsp;• The day number is not between 1-31  
+&nbsp;&nbsp;• The month number is not between 1-12  
+&nbsp;&nbsp;• The year is in the future  
+&nbsp;&nbsp;• The date is invalid e.g. 31/04/1991 (there are 30 days in April)
--- main.js
+++ main.js
@@ -1,2 +1,23 @@
 import "core-js/stable";
 import "regenerator-runtime/runtime";
+
+// VARIABLES
+const dayInput = document.getElementById("day");
+const monthInput = document.getElementById("month");
+const yearInput = document.getElementById("year");
+const formBtn = document.querySelector(".form__btn");
+const yearsCalculation = document.getElementById("calculation-years");
+const monthsCalculation = document.getElementById("calculation-months");
+const daysCalculation = document.getElementById("calculation-days");
+
+// EVENT LISTENER
+formBtn.addEventListener("click", function (e) {
+  const btn = e.target.closest(".form__btn");
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
+  const currentMonth = currentDate.getMonth() + 1;
+  const currentDay = currentDate.getDate();
+  const birthDay = dayInput.value;
+  const birthMonth = monthInput.value;
+  const birthYear = yearInput.value;
+});
