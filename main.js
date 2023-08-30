2c377c2bb8b4fcbb2c0b3493111561524c05c535
AdamHaniff <AdamHaniffDeveloper@gmail.com>
2023-08-30 16:46:08 -0400

    Created addLeadingZero function

--- main.js
+++ main.js
@@ -9,17 +9,21 @@ const formBtn = document.querySelector(".form__btn");
 const yearsCalculation = document.getElementById("calculation-years");
 const monthsCalculation = document.getElementById("calculation-months");
 const daysCalculation = document.getElementById("calculation-days");
+const yearsUnit = document.getElementById("calculation-unit-years");
+const monthsUnit = document.getElementById("calculation-unit-months");
+const daysUnit = document.getElementById("calculation-unit-days");
 
-// EVENT LISTENER CALLBACK FUNCTION
-function handleFormBtnClick(e) {
-  const btn = e.target.closest(".form__btn");
-  if (!btn) return;
+// HELPER FUNCTIONS
+function updateUnitText(element, value, singularText, pluralText) {
+  element.textContent = value === 1 ? singularText : pluralText;
+}
 
-  // Get user input
-  const birthDay = parseInt(dayInput.value);
-  const birthMonth = parseInt(monthInput.value) - 1;
-  const birthYear = parseInt(yearInput.value);
+function addLeadingZero(inputElement) {
+  inputElement.value = inputElement.value.padStart(2, "0");
+}
 
+// FUNCTIONS
+function calculateAge(birthYear, birthMonth, birthDay) {
   // Get current date
   const currentDate = new Date();
   const currentYear = currentDate.getFullYear();
@@ -47,10 +51,38 @@ function handleFormBtnClick(e) {
     months += 12;
   }
 
-  // Display result
+  return { years, months, days };
+}
+
+function displayAgeCalculations(years, months, days) {
+  // Display calculations
   yearsCalculation.textContent = years;
   monthsCalculation.textContent = months;
   daysCalculation.textContent = days;
+
+  // Check if units are equal to 1
+  updateUnitText(yearsUnit, years, "year", "years");
+  updateUnitText(monthsUnit, months, "month", "months");
+  updateUnitText(daysUnit, days, "day", "days");
+}
+
+// EVENT LISTENER CALLBACK FUNCTION
+function handleFormBtnClick(e) {
+  const formBtn = e.target.closest(".form__btn");
+  if (!formBtn) return;
+
+  // Add a leading 0 to input elements
+  addLeadingZero(dayInput);
+  addLeadingZero(monthInput);
+
+  // Get user input
+  const birthDay = parseInt(dayInput.value);
+  const birthMonth = parseInt(monthInput.value) - 1;
+  const birthYear = parseInt(yearInput.value);
+
+  // Calculate and display age
+  const { years, months, days } = calculateAge(birthYear, birthMonth, birthDay);
+  displayAgeCalculations(years, months, days);
 }
 
 // EVENT LISTENER
